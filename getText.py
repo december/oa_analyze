679adb0618cd23fc167425e4adf69693ce1524e2
路云飞 <luyunfeicx@163.com>
2016-11-15 14:18:38 +0800

    fix bug

--- getText.py
+++ getText.py
@@ -27,7 +27,7 @@ def delta(begin, now):
 	d = n - b
 	return d.days
 
-csvfile = file('mnt/data5/luyunfei/rawdata/Wechat_OA/10059_all_mod1k.csv', 'r')
+csvfile = file('/mnt/data5/luyunfei/rawdata/Wechat_OA/10059_all_mod1k.csv', 'r')
 reader = csv.reader(csvfile)
 comelist = list()
 namelist = list()
