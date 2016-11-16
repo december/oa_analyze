c3894d8ec2b36133a31930c4edf8b49fd14a814e
路云飞 <luyunfeicx@163.com>
2016-11-16 15:09:05 +0800

    fix bug

--- getQ.py
+++ getQ.py
@@ -35,9 +35,9 @@ for item in namelist:
 		x = list()
 		y = list()
 		for i in range(idb, ide - 1):
-			x.append(int(singlefile[2][i]))
-		for i in range(idb + 1, ide):
-			y.append(int(singlefile[2][i]))
+			if (int(singlefile[2][i]) > 0 and int(singlefile[2][i+1]) > 0):
+				x.append(int(singlefile[2][i]))
+				y.append(int(singlefile[2][i+1]))
 
 		x = np.array(x)
 		y = np.array(y)
